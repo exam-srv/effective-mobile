e8fb88ae9b45a416abe1491a0a8b407228b0c479
exam-srv <ethrave1@rambler.ru>
2026-05-08 23:51:59 +0300

    Проверка deploy action

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Effective Mobile!!!');
+  res.send('Hello from Effective Mobile!!! Если вы это читаете deploy прошёл успешно');
 });
 
 app.listen(port, '0.0.0.0', () => {
